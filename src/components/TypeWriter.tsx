ef76337b87ebb7279f6e1816d2c1d4dd53c1f25b
Joe Fong <kapppa.joe@gmail.com>
2021-11-15 20:23:58 +0000

    add typewriter effect to intro msg

--- src/components/TypeWriter.tsx
+++ src/components/TypeWriter.tsx
@@ -11,28 +11,47 @@ interface props {
   showCursor?: boolean
 }
 
+const addLineBreak = (stringArr:string[]): (string|JSX.Element)[] => {
+  let result = [];
+  for (let i = 0; i < stringArr.length; i++) {
+    result.push(stringArr[i])
+    if (i !== stringArr.length - 1) {
+      result.push(<br />)
+    }
+  }
+  return result;
+}
 
 const TypeWriterCursor = () => {
   return (<span className="typewriter-cursor">|</span>)
 }
 
 const TypeWriter = ({ str = "", delayTime = 50, play = true, showCursor =  true }:props) => {
-  const [text, setText] = useState("");
-  const startTyping = async () => {
+  const [text, setText] = useState<Array<string>>([]);
+  const startTyping = async (ref:{isMounted: boolean}) => {
     for (let i = 0; i < str.length; i++) {
-      const nextText = str.slice(0, i + 1);
+      let nextText = str.slice(0, i + 1).split("\n");
       await delay(delayTime);
-      setText(nextText);
+      if (ref.isMounted) {
+        setText(nextText);
+      }
     }
   };
+
   useEffect(() => {
+    const ref = {isMounted: true}
     if (play) {
-      startTyping();
+      startTyping(ref);
+    }
+    return () => {
+      ref.isMounted = false;
     }
   }, [play]);
 
   return (
-    <><span className="typewriter-text">{text}</span>{showCursor && <TypeWriterCursor />}</>
+    <><span className="typewriter-text">
+      {text.length > 1 ? addLineBreak(text) : text}
+    </span>{showCursor && <TypeWriterCursor />}</>
   );
 };
 
--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -1,8 +1,13 @@
+import TypeWriter from "../components/TypeWriter";
+
+const welcomeMsg = "Hey, nice to meet you :)\n" + "> My name is Joe Fong."
+
 const Intro = () => {
   return (
     <section id="intro">
-      {`> Hey, nice to meet you :)`}
-      {`> My name is Joe Fong.|`}
+      <div className="welcome-msg">{`> `}<TypeWriter str={welcomeMsg} delayTime={100} /></div>
+      {/* {`> Hey, nice to meet you :)`}
+      {`> My name is Joe Fong.`} */}
     </section>
   );
 };
