d4e5bb2c84d6aec96978c837e764aaddf73dd512
Joe Fong <kapppa.joe@gmail.com>
2021-11-17 13:18:42 +0000

    set up carousel for #Projects

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,14 +1,28 @@
 import ProjectCard from "../components/ProjectCard";
 import ProjectData from "../data/ProjectData";
+import 'preact/compat'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel/lib/js';
+import useWindowDimensions from "../hooks/useWindowDimensions";
+
+const carouselSettings = {
+  centerMode: true,
+  centerSlidePercentage: "70%",
+  infiniteLoop: true
+}
+
+const tabletBreakpoint = 768
 
 const Projects = () => {
+  const { width } = useWindowDimensions()
+
   return (
     <section id="projects">
       <h2 className="section-title">Projects</h2>
       <p className="subtitle">Some of the things I've made...</p>
-
-      {ProjectData.map(project => <ProjectCard {...project} />)}
-
+      <Carousel infiniteLoop={true} centerMode={width > tabletBreakpoint} centerSlidePercentage="70">
+        {ProjectData.map(project => <ProjectCard {...project} />)}
+      </Carousel>
     </section>
   );
 };
