f25e39c35ef17398cc43108bca0d7e032c7c3019
Joe Fong <kapppa.joe@gmail.com>
2021-11-17 16:35:10 +0000

    commit before try to add scrolling behaviour to nav bar

--- src/app.tsx
+++ src/app.tsx
@@ -1,26 +1,28 @@
-import { useState } from 'preact/hooks'
-
-import NavBar from './components/NavBar'
-import Intro from './sections/Intro'
-import AboutMe from './sections/AboutMe'
-import Skills from './sections/Skills'
-import Projects from './sections/Projects'
-import Contact from './sections/Contact'
-import ThemeToggleButton from './components/ThemeToggleButton'
+import { useState } from "preact/hooks";
+import { BrowserRouter } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Intro from "./sections/Intro";
+import AboutMe from "./sections/AboutMe";
+import Skills from "./sections/Skills";
+import Projects from "./sections/Projects";
+import Contact from "./sections/Contact";
+import ThemeToggleButton from "./components/ThemeToggleButton";
 
 export function App() {
-  const [isDarkMode, setIsDarkMode] = useState(true)
-  const toggleTheme = () => setIsDarkMode(curr => !curr)
-  
+  const [isDarkMode, setIsDarkMode] = useState(true);
+  const toggleTheme = () => setIsDarkMode((curr) => !curr);
+
   return (
-    <div id="app" data-theme={isDarkMode ? 'dark' : 'light'}>
-      <ThemeToggleButton isDarkMode={isDarkMode} toggleTheme={toggleTheme}/>
-      <NavBar />
-      <Intro />
-      <AboutMe />
-      <Skills />
-      <Projects />
-      <Contact />
-    </div>
-  )
+    <BrowserRouter>
+      <div id="app" data-theme={isDarkMode ? "dark" : "light"}>
+        <ThemeToggleButton isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
+        <NavBar />
+        <Intro />
+        <AboutMe />
+        <Skills />
+        <Projects />
+        <Contact />
+      </div>
+    </BrowserRouter>
+  );
 }
--- src/components/NavButton.tsx
+++ src/components/NavButton.tsx
@@ -1,9 +1,9 @@
-import {useState} from 'preact/hooks';
-import TypeWriter from './TypeWriter';
+import { useState } from "preact/hooks";
+import TypeWriter from "./TypeWriter";
 
 export interface NavButtonProps {
-  str: string
-  linkTo: string
+  str: string;
+  linkTo: string;
 }
 
 const makeCodingStyleString = (str: string): string => {
@@ -11,36 +11,51 @@ const makeCodingStyleString = (str: string): string => {
     const [a, b] = str.toLowerCase().split(" ");
     return `.${a}(${b})`;
   } else {
-    return `.${str.toLowerCase()}()`
+    return `.${str.toLowerCase()}()`;
   }
-}
+};
 
-const NavButton = ({str, linkTo}: NavButtonProps) => {
-  const [isActive, setIsActive] = useState(false)
-  
-  return (<a href={linkTo} className="nav-button" onFocus={() => setIsActive(true)} onMouseEnter={() => setIsActive(true)} onBlur={() => setIsActive(false)} onMouseLeave={() => setIsActive(false)}>
+const NavButton = ({ str, linkTo }: NavButtonProps) => {
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <a
+      href={linkTo}
+      className="nav-button"
+      onFocus={() => setIsActive(true)}
+      onMouseEnter={() => setIsActive(true)}
+      onBlur={() => setIsActive(false)}
+      onMouseLeave={() => setIsActive(false)}
+    >
       <NavButtonContent str={str} isActive={isActive} />
-    </a>)
+    </a>
+  );
 };
 
 interface NavButtonContentProps {
-  isActive: boolean,
-  str: string
+  isActive: boolean;
+  str: string;
 }
 
-
-const NavButtonContent = ({isActive, str}: NavButtonContentProps) => {
+const NavButtonContent = ({ isActive, str }: NavButtonContentProps) => {
   if (isActive) {
     return (
-      <span className="nav-button-text" data-req-width={`${str.length * 10 + 42}px`}>
+      <span
+        className="nav-button-text"
+        data-req-width={`${str.length * 10 + 42}px`}
+      >
         <TypeWriter str={makeCodingStyleString(str)} />
       </span>
     );
   } else {
     return (
-      <span className="nav-button-text" data-req-width={`${str.length * 10 + 42}px`}>
+      <span
+        className="nav-button-text"
+        data-req-width={`${str.length * 10 + 42}px`}
+      >
         {str}
-      </span>)
+      </span>
+    );
   }
 };
 
