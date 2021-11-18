e9e3bed7a532282025ff1ea35217f6d7d623b1a9
Joe Fong <kapppa.joe@gmail.com>
2021-11-18 18:24:33 +0000

    update skill bars

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Background pattern image sourced from [Subtle Patterns](https://www.toptal.com/designers/subtlepatterns/)
+
+Sushi by James Christopher from the Noun Project
+https://thenounproject.com/term/sushi/7845/
--- src/data/SkillData.ts
+++ src/data/SkillData.ts
@@ -8,14 +8,16 @@ export type SkillDataType = {
 export const codingSkills: SkillDataType[] = [
   {name: 'Javascript', slug: 'javascript', value: 0.8, description: "Pretty good at this"}
   ,{name: 'Python', slug: 'python', value: 0.55, description: "Intermediate"}
-  ,{name: 'React', slug: 'react', value: 0.3, description: "Beginner"}
+  ,{name: 'React', slug: 'react', value: 0.35, description: "Still much more to learn"}
   ,{name: 'HTML/CSS', slug: 'code', value: 0.55, description: "Intermediate"}
-  ,{name: 'Ruby', slug: 'ruby', value: 0.3, description: "Beginner"}
+  ,{name: 'SASS', slug: 'sass', value: 0.25, description: "Beginner"}
+  ,{name: 'Ruby', slug: 'ruby', value: 0.25, description: "Beginner"}
   ,{name: 'Nim', slug: 'nim', value: 0.55, description: "Some experiences"}
   ,{name: 'VBA (Excel)', slug: 'code', value: 0.55, description: "Some experiences"}
 ]
 
 export const otherSkills: SkillDataType[] =  [
   {name: 'Linux', slug: 'linux', value: 0.50, description: "Not a guru but using it daily"}
-  ,{name: 'git', slug: 'git', value: 0.35, description: "Basic operations"}
+  ,{name: 'git', slug: 'git', value: 0.40, description: "Basic operations"}
+  ,{name: 'sushi eating', slug: 'sushi', value: 0.9, description: "Can eat a lot"}
 ]
