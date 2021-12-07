585cbba62fa4b23635130d8c3ab613b20496eefd
Joe Fong <kapppa.joe@gmail.com>
2021-12-07 11:58:40 +0000

    revise about-me text

--- src/sections/AboutMe.tsx
+++ src/sections/AboutMe.tsx
@@ -1,10 +1,10 @@
-const contentText = `Hi, I am <em>Joe Fong</em>. A curiosity-driven guy who has a little hobby of coding.
+const contentText = `Hi, I am <em>Joe Fong</em>. A curious guy who like to explore new things.
 Having graduated from the coding boot camp <em>#Northcoders</em> recently, I am now ready to embark on my journey as a software developer.
 
 My previous workplace - a Japanese electronic firm - had piles of unmaintained legacy excel files and VBA macros which we had to run every day. Tinkering and debugging those legacy macros has sparked my interest in coding. Since then, I have developed a little hobby of teaching myself how to code. 
 Although my job position wasn't much related to software development, I have always liked to adopt the things I learned to solve some daily problems. I built some Excel <em>VBA macros</em> and little utility scripts in <em>Python</em> to speed up my routine office works.
 Aside from work, I also enjoyed bashing a kata on <em>Codewars</em> or tackling a challenge from <em>Project Euler</em>.
-<strong>Learning something new and tinkering with it never fails to fill me with delight, and that's what sparks me to pursue a career as a Software Developer</strong>`;
+<strong>Learning something new and tinkering with it never fails to fill me with delight, and that's what sparks me to pursue a career as a Software Developer.</strong>`;
 
 const contents = contentText.split("\n").map((sentence) => {
   return <p dangerouslySetInnerHTML={{ __html: sentence }}></p>;
